fd9acb95e1ac48d42c19a7da0f555676d5fe8ee6
przybjul <104941410+przybjul@users.noreply.github.com>
2022-12-16 20:57:30 +0100

    detect label and moderation

--- rekognition.py
+++ rekognition.py
@@ -8,13 +8,25 @@ with open('new_user_credentials.csv', 'r') as input:
         access_key_id = line[2]
         secret_access_key = line[3]
 
-photo = 'hot_air_baloon.jpg'
+photo = 'beyonce.jpg'
 
 client = boto3.client('rekognition', aws_access_key_id = access_key_id, aws_secret_access_key = secret_access_key, region_name='us-west-2')
 
 with open(photo, 'rb') as source_image:
     source_bytes = source_image.read()
 
-response = client.detect_labels(Image={'Bytes':source_bytes}, MaxLabels = 2, MinConfidence=95)
+# response = client.detect_labels(Image={'Bytes':source_bytes}, MaxLabels = 2, MinConfidence=95)
+
+#rozpoznanie rodzaju obiektu
+response = client.detect_labels(Image={'S3Object':{
+    "Bucket":"image-recognition-1",
+    "Name":photo
+    }}, MaxLabels = 2, MinConfidence=95)
+
+#moderacja - treści niewłaściwe takie jak nudity
+response = client.detect_moderation_labels(Image={'S3Object':{
+    "Bucket":"image-recognition-1",
+    "Name":photo
+    }},MinConfidence=90)
 
 print(response)
